5b261ae9abff7d483a06e0a6f8cd72887dcf1c99
Hunter104 <000plagueinc@gmail.com>
2024-05-29 17:28:30 -0300

    adicionados códigos de erro e leitura de arquivo

--- lexico.cpp
+++ lexico.cpp
@@ -1,14 +1,17 @@
 #include <cctype>
+#include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 #define CODIGO_IDENTIFICADOR -1
 #define CODIGO_NUMERO -2
 #define CODIGO_ERRO -99
 
 const std::unordered_map<std::string, int> simbolos_especiais = {
-    {".", 1},  {":", 2},   {",", 3},   {"(", 4},   {")", 5},  {"=", 6},
-    {"<", 7},  {">", 8},   {"+", 9},   {"-", 10},  {"*", 11}, {"[", 12},
-    {"]", 13}, {":=", 14}, {"..", 15}, {"(*", 16}, {"*)", 17}};
+    {".", 1},   {":", 2},   {",", 3},  {"(", 4},   {")", 5},
+    {"=", 6},   {"<", 7},   {">", 8},  {"+", 9},   {"-", 10},
+    {"*", 11},  {"[", 12},  {"]", 13}, {":=", 14}, {"..", 15},
+    {"(*", 16}, {"*)", 17}, {";", 18}, {"'", 19}};
 const std::unordered_map<std::string, int> palavras_chave = {
     {"program", 100}, {"label", 101}, {"type", 102},      {"array", 103},
     {"of", 104},      {"var", 105},   {"procedure", 106}, {"function", 107},
@@ -21,17 +24,38 @@ struct token {
   int code;
 };
 
-int main() {}
+token analisador_lexico(std::string::iterator &prox,
+                        const std::string::iterator &end);
+
+int main() {
+  std::string buffer;
+  std::string source_code;
+  while (std::getline(std::cin, buffer))
+    source_code += buffer;
+
+  std::cout << source_code << '\n';
+  auto it = source_code.begin();
+  std::vector<token> tokens;
+  while (it != source_code.end()) {
+    token t = analisador_lexico(it, source_code.end());
+    if (t.code == -99) {
+      std::cout << t.code << " - " << t.content << '\n';
+      break;
+    }
+    tokens.push_back(t);
+    std::cout << t.code << " - " << t.content << '\n';
+  }
+}
 
 token analisador_lexico(std::string::iterator &prox,
-                        std::string::iterator &end) {
+                        const std::string::iterator &end) {
   std::string atom;
 
   while (prox != end && *prox == ' ')
     prox++;
 
   if (prox == end)
-    return {"", CODIGO_ERRO};
+    return {"Fim de cadeia.", CODIGO_ERRO};
 
   std::string s(1, *prox);
   if (simbolos_especiais.find(s) != simbolos_especiais.end()) {
@@ -64,10 +88,10 @@ token analisador_lexico(std::string::iterator &prox,
       prox++;
     }
     if (prox != end && islower(*prox)) {
-      return {"", CODIGO_ERRO};
+      return {"Letra em meio de numero.", CODIGO_ERRO};
     }
     return {atom, CODIGO_NUMERO};
   }
 
-  return {"", CODIGO_ERRO};
+  return {"Token inesperado.", CODIGO_ERRO};
 }
