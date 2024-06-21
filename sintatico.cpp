6afeac8e43d6c9b3e07a89edb97e12b4ec8a828c
Hunter104 <000plagueinc@gmail.com>
2024-06-21 12:54:10 -0300

    início da análise sintática

--- sintatico.cpp
+++ sintatico.cpp
@@ -1,76 +1,89 @@
 #include <cctype>
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <ostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 const std::vector<std::string> tokenTypeNames = {
-    "Empty token", "Error token",  "Identifier token",      "Number token",
-    "Eof token",   "Symbol token", "Compound symbol token", "Keyword token",
-};
+    "KEYWORD",           "IDENTIFIER", "NUMBER",   "OPERATOR",
+    "COMPOUND OPERATOR", "DELIMITER",  "COMMENTS", "UNKNOWN"};
 
 enum TokenType {
-  TOKENTYPE_EMPTY,
-  TOKENTYPE_ERROR,
+  TOKENTYPE_KEYWORD,
   TOKENTYPE_IDENTIFIER,
   TOKENTYPE_NUMBER,
-  TOKENTYPE_EOF,
-  TOKENTYPE_SYMBOL,
-  TOKENTYPE_COMPOUNDSYMBOL,
-  TOKENTYPE_KEYWORD,
+  TOKENTYPE_OPERATOR,
+  TOKENTYPE_COMPOUND_OPERATOR,
+  TOKENTYPE_DELIMITER,
+  TOKENTYPE_COMMENTS,
+  TOKENTYPE_UNKNOWN,
+  TOKENTYPE_ERROR
 };
 
 enum TokenCode {
-  // Tokens especiais 0xx:
+  // Tokens especiais:
   TOKEN_EMPTY = 0,  // 0 - (Empty token)
   TOKEN_ERROR,      // 1 - (Error token)
   TOKEN_IDENTIFIER, // 2 - (Identifier token)
   TOKEN_NUMBER,     // 3 - (Number token)
   TOKEN_EOF,        // 4 - (Token for end of file)
+  TOKEN_COMMENTS,   // 5 - (Token for comments)
+  TOKEN_UNKNOWN,    // 6 - (Unknown token)
 
-  // Simbolos Especiais 1xx:
-  TOKEN_DOT = 100,  // 100 - .
-  TOKEN_COLON,      // 101 - :
-  TOKEN_COMMA,      // 102 - ,
-  TOKEN_LPAREN,     // 103 - (
-  TOKEN_RPAREN,     // 104 - )
-  TOKEN_EQUAL,      // 105 - =
-  TOKEN_LT,         // 106 - <
-  TOKEN_GT,         // 107 - >
-  TOKEN_PLUS,       // 108 - +
-  TOKEN_MINUS,      // 109 - -
-  TOKEN_STAR,       // 110 - *
-  TOKEN_LBRACKET,   // 111 - [
-  TOKEN_RBRACKET,   // 112 - ]
-  TOKEN_SEMICOLON,  // 113 - ;
-  TOKEN_APOSTROPHE, // 114 - '
+  // Operadores OPERATOR:
+  TOKEN_PLUS = 100,  // 100 - +
+  TOKEN_MINUS,       // 101 - -
+  TOKEN_ASTERISK,    // 102 - *
+  TOKEN_SLASH,       // 103 - /
+  TOKEN_LESSTHAN,    // 104 - <
+  TOKEN_GREATERTHAN, // 105 - >
+  TOKEN_EQUAL,       // 106 - =
 
-  // Simbolos Especiais Compostos 2xx:
+  // Operadores compostos COMPOUND_OPERATOR:
   TOKEN_ASSIGNMENT = 200, // 200 - :=
-  TOKEN_SUBRANGE,         // 201 - ..
-  TOKEN_LCOMMENT,         // 202 - (*
-  TOKEN_RCOMMENT,         // 203 - *)
+  TOKEN_LESSEQUAL,        // 201 - <=
+  TOKEN_GREATEREQUAL,     // 202 - >=
+  TOKEN_NOTEQUAL,         // 203 - <>
 
-  // Palavras-chave 3xx:
-  TOKEN_PROGRAM = 300, // 301 - program
-  TOKEN_LABEL,         // 302 - label
-  TOKEN_TYPE,          // 303 - type
-  TOKEN_ARRAY,         // 304 - array
-  TOKEN_OF,            // 305 - of
-  TOKEN_VAR,           // 306 - var
-  TOKEN_PROCEDURE,     // 307 - procedure
-  TOKEN_FUNCTION,      // 308 - function
-  TOKEN_BEGIN,         // 309 - begin
-  TOKEN_END,           // 310 - end
-  TOKEN_IF,            // 311 - if
-  TOKEN_THEN,          // 312 - then
-  TOKEN_ELSE,          // 313 - else
-  TOKEN_WHILE,         // 314 - while
-  TOKEN_DO,            // 315 - do
-  TOKEN_OR,            // 316 - or
-  TOKEN_DIV,           // 317 - div
-  TOKEN_NOT            // 318 - not
+  // Delimitadores DELIMITER:
+  TOKEN_LPARENTHESIS = 300, // 300 - (
+  TOKEN_RPARENTHESIS,       // 301 - )
+  TOKEN_LBRACKET,           // 302 - [
+  TOKEN_RBRACKET,           // 303 - ]
+  TOKEN_SEMICOLON,          // 304 - ;
+  TOKEN_COMMA,              // 305 - ,
+  TOKEN_COLON,              // 306 - :
+  TOKEN_PERIOD,             // 307 - .
+  TOKEN_APOSTROPHE,         // 308 - '
+  TOKEN_QUOTES,             // 309 - "
+
+  // Palavras-chave KEYWORD:
+  TOKEN_AND = 400, // 400 - and
+  TOKEN_ARRAY,     // 401 - array
+  TOKEN_BEGIN,     // 402 - begin
+  TOKEN_DIV,       // 403 - div
+  TOKEN_DO,        // 404 - do
+  TOKEN_ELSE,      // 405 - else
+  TOKEN_END,       // 406 - end
+  TOKEN_FUNCTION,  // 407 - function
+  TOKEN_GOTO,      // 408 - goto
+  TOKEN_IF,        // 409 - if
+  TOKEN_LABEL,     // 410 - label
+  TOKEN_NOT,       // 411 - not
+  TOKEN_OF,        // 412 - of
+  TOKEN_OR,        // 413 - or
+  TOKEN_PROCEDURE, // 414 - procedure
+  TOKEN_PROGRAM,   // 415 - program
+  TOKEN_THEN,      // 416 - then
+  TOKEN_TYPE,      // 417 - type
+  TOKEN_VAR,       // 418 - var
+  TOKEN_WHILE,     // 419 - while
+
+  TOKEN_READ, // 420 - read
+  TOKEN_WRITE // 421 - write
 };
 
 struct token {
@@ -79,46 +92,44 @@ struct token {
 };
 
 TokenType getTokenType(TokenCode code) {
-  switch (code) {
-  case TOKEN_EMPTY:
-    return TOKENTYPE_EMPTY;
-  case TOKEN_ERROR:
-    return TOKENTYPE_ERROR;
-  case TOKEN_IDENTIFIER:
+  if (code == TOKEN_IDENTIFIER)
     return TOKENTYPE_IDENTIFIER;
-  case TOKEN_NUMBER:
+  if (code == TOKEN_NUMBER)
     return TOKENTYPE_NUMBER;
-  case TOKEN_EOF:
-    return TOKENTYPE_EOF;
-  }
-
-  if (code < 200 && code >= 100)
-    return TOKENTYPE_SYMBOL;
-
-  if (code < 300 && code >= 200)
-    return TOKENTYPE_COMPOUNDSYMBOL;
-
-  if (code < 400 && code >= 300)
+  if (code == TOKEN_COMMENTS)
+    return TOKENTYPE_COMMENTS;
+  if (code == TOKEN_UNKNOWN)
+    return TOKENTYPE_UNKNOWN;
+  if (code == TOKEN_ERROR)
+    return TOKENTYPE_ERROR;
+  if (code >= 400)
     return TOKENTYPE_KEYWORD;
-
-  return TOKENTYPE_ERROR;
+  if (code >= 300 && code < 400)
+    return TOKENTYPE_DELIMITER;
+  if (code >= 200 && code < 300)
+    return TOKENTYPE_COMPOUND_OPERATOR;
+  if (code >= 100 && code < 200)
+    return TOKENTYPE_OPERATOR;
+  return TOKENTYPE_UNKNOWN;
 }
 
 const std::unordered_map<std::string, TokenCode> simbolos_especiais = {
-    {".", TOKEN_DOT},       {":", TOKEN_COLON},       {",", TOKEN_COMMA},
-    {"(", TOKEN_LPAREN},    {")", TOKEN_RPAREN},      {"=", TOKEN_EQUAL},
-    {"<", TOKEN_LT},        {">", TOKEN_GT},          {"+", TOKEN_PLUS},
-    {"-", TOKEN_MINUS},     {"*", TOKEN_STAR},        {"[", TOKEN_LBRACKET},
-    {"]", TOKEN_RBRACKET},  {":=", TOKEN_ASSIGNMENT}, {"..", TOKEN_SUBRANGE},
-    {"(*", TOKEN_LCOMMENT}, {"*)", TOKEN_RCOMMENT},   {";", TOKEN_SEMICOLON},
-    {"'", TOKEN_APOSTROPHE}};
+    {".", TOKEN_PERIOD},       {":", TOKEN_COLON},
+    {",", TOKEN_COMMA},        {"(", TOKEN_LPARENTHESIS},
+    {")", TOKEN_RPARENTHESIS}, {"=", TOKEN_EQUAL},
+    {"<", TOKEN_LESSTHAN},     {">", TOKEN_GREATERTHAN},
+    {"+", TOKEN_PLUS},         {"-", TOKEN_MINUS},
+    {"*", TOKEN_ASTERISK},     {"[", TOKEN_LBRACKET},
+    {"]", TOKEN_RBRACKET},     {":=", TOKEN_ASSIGNMENT},
+    {"<=", TOKEN_LESSEQUAL},   {">=", TOKEN_GREATEREQUAL},
+    {"<>", TOKEN_NOTEQUAL},    {";", TOKEN_SEMICOLON},
+    {"/", TOKEN_SLASH}};
 
 const std::unordered_map<std::string, TokenCode> palavras_chave = {
     {"program", TOKEN_PROGRAM},
     {"label", TOKEN_LABEL},
     {"type", TOKEN_TYPE},
     {"array", TOKEN_ARRAY},
-    {"of", TOKEN_OF},
     {"var", TOKEN_VAR},
     {"procedure", TOKEN_PROCEDURE},
     {"function", TOKEN_FUNCTION},
@@ -129,15 +140,20 @@ const std::unordered_map<std::string, TokenCode> palavras_chave = {
     {"else", TOKEN_ELSE},
     {"while", TOKEN_WHILE},
     {"do", TOKEN_DO},
-    {"or", TOKEN_OR},
     {"div", TOKEN_DIV},
-    {"not", TOKEN_NOT}};
+    {"and", TOKEN_AND},
+    {"goto", TOKEN_GOTO},
+    {"read", TOKEN_READ},
+    {"write", TOKEN_WRITE},
+    {"not", TOKEN_NOT},
+    {"of", TOKEN_OF},
+    {"or", TOKEN_OR}};
 
 token analisador_lexico(std::string::iterator &prox,
                         const std::string::iterator &end) {
   std::string atom;
-  for (; prox != end && *prox == ' '; prox++)
-    ;
+  while (prox != end && isspace(*prox))
+    prox++;
 
   if (prox == end)
     return {"#", TOKEN_EOF};
@@ -148,16 +164,38 @@ token analisador_lexico(std::string::iterator &prox,
     if (s == ":" && *prox == '=') {
       s = ":=";
       prox++;
-    } else if (s == "." && *prox == '.') {
-      s = "..";
+    } else if (s == "<" && *prox == '>') {
+      s = "<>";
       prox++;
+    } else if (s == "<" && *prox == '=') {
+      s = "<=";
+      prox++;
+    } else if (s == ">" && *prox == '=') {
+      s = ">=";
+      prox++;
+    } else if (s == "(" && *prox == '*') {
+      s = "(*";
+      prox++;
+      auto temp = prox;
+      while (temp != end) {
+        if ((temp + 1) != end && *temp == '*' && *(temp + 1) == ')') {
+          prox = temp + 2;
+          return {s + "*)", TOKEN_COMMENTS}; // EXPLODE COMENTARIO
+        }
+        s.push_back(*temp);
+        temp++;
+      }
+      prox--;
+      return {"(", TOKEN_LPARENTHESIS};
     }
     return {s, simbolos_especiais.find(s)->second};
   }
 
   if (isalpha(*prox)) {
-    while (prox != end && (isalpha(*prox) || isdigit(*prox)))
-      atom.push_back(*prox++);
+    while (prox != end && (isalpha(*prox) || isdigit(*prox))) {
+      atom.push_back(*prox);
+      prox++;
+    }
     if (palavras_chave.find(atom) != palavras_chave.end())
       return {atom, palavras_chave.find(atom)->second};
     else
@@ -165,14 +203,18 @@ token analisador_lexico(std::string::iterator &prox,
   }
 
   if (isdigit(*prox)) {
-    while (prox != end && isdigit(*prox))
+    bool rational_part = false;
+    while (prox != end && (isdigit(*prox) || (*prox == '.' && !rational_part &&
+                                              isdigit(*(prox + 1))))) {
+      if (*prox == '.')
+        rational_part = true;
       atom.push_back(*prox++);
-    if (prox != end && isalpha(*prox))
-      return {"Letra em meio de numero.", TOKEN_ERROR};
-    else
-      return {atom, TOKEN_NUMBER};
+    }
+    return {atom, TOKEN_NUMBER};
   }
-  return {"Token inesperado.", TOKEN_ERROR};
+
+  prox++;
+  return {s, TOKEN_UNKNOWN};
 }
 
 std::vector<token> getTokens(std::string source_code) {
@@ -181,86 +223,62 @@ std::vector<token> getTokens(std::string source_code) {
 
   do {
     tokens.push_back(analisador_lexico(it, source_code.end()));
+    if (tokens.back().code == TOKEN_ERROR) {
+      std::cerr << tokens.back().content << '\n';
+      exit(1);
+    }
   } while (tokens.back().code != TOKEN_EOF);
   return tokens;
 }
 
-std::string read_source_file() {
+std::string read_source_file(std::string file_path) {
+  std::ifstream file(file_path);
+  if (!file.is_open()) {
+    std::cerr << "Erro awo abrir arquivo: " << file_path << std::endl;
+    exit(1);
+  }
   std::string buffer;
   std::string source_code;
-  while (std::getline(std::cin, buffer))
-    source_code += buffer;
+  while (std::getline(file, buffer))
+    source_code += buffer + '\n';
 
+  file.close();
   return source_code;
 }
 
-std::map<TokenType, int> countTokenTypes(std::vector<token> tokens) {
-
-  std::map<TokenType, int> counts = {{TOKENTYPE_EMPTY, 0},
-                                     {TOKENTYPE_ERROR, 0},
+std::map<TokenType, int> countTokenTypes(const std::vector<token> &tokens) {
+  std::map<TokenType, int> counts = {{TOKENTYPE_KEYWORD, 0},
                                      {TOKENTYPE_IDENTIFIER, 0},
                                      {TOKENTYPE_NUMBER, 0},
-                                     {TOKENTYPE_EOF, 0},
-                                     {TOKENTYPE_SYMBOL, 0},
-                                     {TOKENTYPE_COMPOUNDSYMBOL, 0},
-                                     {TOKENTYPE_KEYWORD, 0}};
-  for (token &t : tokens) {
+                                     {TOKENTYPE_OPERATOR, 0},
+                                     {TOKENTYPE_COMPOUND_OPERATOR, 0},
+                                     {TOKENTYPE_DELIMITER, 0},
+                                     {TOKENTYPE_COMMENTS, 0},
+                                     {TOKENTYPE_UNKNOWN, 0}};
+  for (const token &t : tokens) {
     TokenType type = getTokenType(t.code);
-    counts.find(type)->second++;
+    counts[type]++;
   }
-
+  // TIRA O EOF DA CONTAGEM
+  counts[TOKENTYPE_UNKNOWN]--;
   return counts;
 }
 
-void erro() {
-  std::cout << "Erro\n";
-  exit(1);
-}
-
-void lista_de_identificadores(std::vector<token>::iterator simbolo) {
-  if (simbolo->code != TOKEN_IDENTIFIER)
-    erro();
-  simbolo++;
-  while (simbolo->code == TOKEN_COMMA) {
-    simbolo++;
-    if (simbolo->code != TOKEN_IDENTIFIER)
-      erro();
-    simbolo++;
-  }
-}
-
-void bloco() {}
-
-void programa(std::vector<token>::iterator simbolo) {
-  if (simbolo->code != TOKEN_PROGRAM)
-    erro();
-  simbolo++;
-  if (simbolo->code != TOKEN_IDENTIFIER)
-    erro();
-  simbolo++;
-  if (simbolo->code != TOKEN_LPAREN)
-    erro();
-  simbolo++;
-  lista_de_identificadores(simbolo);
-  if (simbolo->code != TOKEN_RPAREN)
-    erro();
-  simbolo++;
-  if (simbolo->code != TOKEN_SEMICOLON)
-    erro();
-  simbolo++;
-  bloco();
-  if (simbolo->code != TOKEN_DOT)
-    erro();
-  simbolo++;
-}
-
-int main() {
-  std::string source_code = read_source_file();
+int main(int argc, char *argv[]) {
+  // argumento 0 é a própia chamada do executável
+  std::string file_path(argv[1]);
+  std::string source_code = read_source_file(file_path);
   std::vector<token> tokens = getTokens(source_code);
+#ifdef DEBUG
+  for (auto &token : tokens)
+    std::cout << tokenTypeNames[getTokenType(token.code)] << "-"
+              << token.content << "\n";
+#endif
   auto counts = countTokenTypes(tokens);
 
   for (auto &count : counts) {
     std::string name = tokenTypeNames[count.first];
-    std::cout << name << " - " << count.second << "\n";
+    std::cout << name << ": " << count.second << "\n";
   }
+  return 0;
 }
